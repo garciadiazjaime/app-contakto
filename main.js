2e14e7a89de643b6b64a972dbe0b5929ac08164c
jaime <garciadiazjaime@gmail.com>
2021-05-12 18:56:30 -0500

    feat: [https://trello.com/c/DIHjMtnK]

--- main.js
+++ main.js
@@ -66,6 +66,47 @@ exports.saveUserFile = saveUserFile
 exports.deleteUserFile = deleteUserFile
 exports.generateZip = generateZip
 
+let myWindow = null
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (myWindow) {
+      if (myWindow.isMinimized()) myWindow.restore()
+      myWindow.focus()
+    }
+  })
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(() => {
+    myWindow = createWindow()
+  })
+
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
+}
+
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
@@ -79,7 +120,6 @@ function createWindow () {
 
   protocol.interceptFileProtocol('file', (request, callback) => {
     const url = request.url.substr(7)
-    console.log('url', url, path.normalize(`${__dirname}/${url}`))
 
     callback({ path: path.normalize(`${__dirname}/bundle/${url}`)})
   })
@@ -90,32 +130,4 @@ function createWindow () {
     protocol: 'file',
     slashes: true
   }))
-  
-  // Open the DevTools. 
-  // win.webContents.openDevTools()
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
